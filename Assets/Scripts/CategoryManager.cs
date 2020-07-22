9e1745c3a24576dc8d4e7a31d765d81029a8d7fe
Abdullah Kirmiziyuz <abdukir@outlook.com>
2020-07-22 22:00:16 +0300

    Category menu

---
+++ Assets/Scripts/CategoryManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CategoryManager : MonoBehaviour
+{
+    public void CallCategory(int ID)
+    {
+        Debug.LogWarning("Category " + ID + " is called");
+    }
+}
